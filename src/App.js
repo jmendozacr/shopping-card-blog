c74a9c90cfd411b6e119db394205163e5cecd170
Josue Mendoza <josuem@thehangar.cr>
2021-04-21 10:21:02 -0600

    adding uuid library, adding some styles and props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { v4 as uuidv4 } from 'uuid';
 import { NavLink, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Blog from './pages/Blog';
@@ -7,6 +8,13 @@ import Shopping from './pages/Shopping';
 import Error404 from './pages/Error404';
 
 function App() {
+    const products = [
+        { id: uuidv4(), name: "apple" },
+        { id: uuidv4(), name: "banana" },
+        { id: uuidv4(), name: "cherry" },
+        { id: uuidv4(), name: "watermelon" },
+    ];
+
     return (
         <Container>
             <Menu>
@@ -18,7 +26,9 @@ function App() {
                 <Switch>
                     <Route path="/" exact component={Home}/>
                     <Route path="/blog" component={Blog}/>
-                    <Route path="/shopping" component={Shopping}/>
+                    <Route path="/shopping">
+						<Shopping products={products}/>
+					</Route>
                     <Route component={Error404}/>
                 </Switch>
             </main>
@@ -30,24 +40,24 @@ function App() {
 }
 
 const Container = styled.div`
-	max-width: 1000px;
-	padding: 40px;
-	width: 90%;
+	background: #fff;
+	border-radius: 10px;
+	box-shadow: 0px 0px 5px rgba(129, 129, 129, 0.1);
 	display: grid;
 	gap: 20px;
 	grid-template-columns: 2fr 1fr;
-	background: #fff;
 	margin: 40px 0;
-	border-radius: 10px;
-	box-shadow: 0px 0px 5px rgba(129, 129, 129, 0.1);
+	max-width: 1000px;
+	padding: 40px;
+	width: 90%;
 `;
 
 const Menu = styled.nav`
-	width: 100%;
-	text-align: center;
 	background: #092c4c;
-	grid-column: span 2;
 	border-radius: 3px;
+	grid-column: span 2;
+	text-align: center;
+	width: 100%;
 
 	a {
 		color: #fff;
--- src/pages/Shopping.js
+++ src/pages/Shopping.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Products from '../components/Products';
 
-const Shopping = () => {
+const Shopping = ({ products }) => {
     return (
         <div>
             <h1>shopping</h1>
+            <Products products={products}/>
         </div>
     );
 }
